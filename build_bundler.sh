7886210c9413fe4bff06c59c706fb4a3a522ab59
Robert Johnson <128016252+Qwer-TeX@users.noreply.github.com>
2024-08-18 16:17:12 -0400

    Enhance to compile if no clang found

--- build_bundler.sh
+++ build_bundler.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 set -xe
-clang -o bundle bundle.c
+
+# Check if clang is available
+if command -v clang > /dev/null 2>&1; then
+    C_COMPILER=clang
+# Check if gcc is available
+elif command -v gcc > /dev/null 2>&1; then
+    C_COMPILER=gcc
+# Default to cc if neither clang nor gcc is available
+else
+    C_COMPILER=cc
+fi
+
+$C_COMPILER -o bundle bundle.c
 
